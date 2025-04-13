52586f018632744765ebf8a67c0d096edf9e38e3
carlospg93git <carlospg93@gmail.com>
2025-04-13 22:41:46 +0200

    Fix: Simplify deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js'
-      }
-    }
-  },
-  base: '/carlosymaria/',
+  base: '',
   server: {
     host: "::",
     port: 8080,
