e8536ebc1298bb7e1c13bfb691e0624a59ecfeaf
carlospg93git <carlospg93@gmail.com>
2025-04-13 22:44:27 +0200

    Fix: Update build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'iife',
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  },
-  base: '',
+  base: '/carlosymaria/',
   server: {
     host: "::",
     port: 8080,
