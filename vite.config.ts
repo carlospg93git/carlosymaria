c6fb6574a1d22a0a145a315725873cba31ee4903
carlospg93git <carlospg93@gmail.com>
2025-04-13 17:45:39 +0200

    Fix syntax in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+
   base: "/",
   server: {
     host: "::",
@@ -11,7 +12,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
+});
