89e8439d6611627d4f26a0df46787673e8aabba1
carlospg93git <carlospg93@gmail.com>
2025-04-13 22:47:32 +0200

    Fix: Restore GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  base: '/carlosymaria/',
+  base: '',
   server: {
     host: "::",
     port: 8080,
