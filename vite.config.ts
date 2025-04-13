c7ee99d4874d03359e6dd8a4f8c4d8b213d4be1b
carlospg93git <carlospg93@gmail.com>
2025-04-13 22:26:18 +0200

    Fix: Update configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  base: "/",
+  base: '/carlosymaria/',
   server: {
     host: "::",
     port: 8080,
