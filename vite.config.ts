2a9725d5b4eb5d9ed7a39564f073a722473317c0
carlospg93git <carlospg93@gmail.com>
2025-04-13 22:43:47 +0200

    Fix: Update script loading strategy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'iife',
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
   base: '',
   server: {
     host: "::",
