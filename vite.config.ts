c94d1599e09eb9193a8264289e43a474928d0acb
carlospg93git <carlospg93@gmail.com>
2025-04-13 22:46:11 +0200

    Switch to Netlify for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   base: '/carlosymaria/',
   server: {
     host: "::",
